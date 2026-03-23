30722ef8a57188e5670d85d24647840a5d5a4de3
Tran Nhat Khanh <Khanh.Tn2416951@sis.hust.edu.vn>
2026-03-23 22:57:29 +0700

    day3s

--- day3-containsduplicate/containsduplicate.cpp
+++ day3-containsduplicate/containsduplicate.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for(int i=0;i<nums.size();i++){
+    bool containsDuplicate(vector<int>& nums) {//khai bao ham tra ve bool, tham so truyen vao la mang vector tham chieu
+        unordered_set<int> seen;//khai bao hash set de luu cac gia tri da xuat hien
+        for(int i=0;i<nums.size();i++){//duyet qua mang
             if(seen.find(nums[i])!=seen.end()){
-                return true;
+                return true;//neu gia tri da xuat hien trong set thi tra ve true
             }
             else{
-                seen.insert(nums[i]);
+                seen.insert(nums[i]);//neu gia tri chua xuat hien thi them vao set
             }
         }
-        return false;
+        return false;//duyet het mang ma khong co thi tra ve false
     }
 };
 int main(){
